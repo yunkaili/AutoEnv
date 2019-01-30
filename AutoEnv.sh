6f435abd648a55f581e6a6a8275f32c2702ffe0a
liyunkai <yunkai.li@hotmail.com>
2019-01-30 20:15:28 +0800

    fix some linux bugs and fix download bugs

--- AutoEnv.sh
+++ AutoEnv.sh
@@ -7,51 +7,27 @@ case "$(uname -s)" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
-# isOSX
-function isOSX() {
-  if [ ${machine} = 'Mac' ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-# isLinux
-function isLinux() {
-  if [ ${machine} = 'Mac' ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-if ( ! isOSX ) && ( ! isLinux ); then
+if [ ${machine} = "Mac" ]; then
+  isOSX=1
+  isLinux=0
+elif [ ${machine} = "Linux" ]; then
+  isOSX=0
+  isLinux=1
+else
   echo "Support Mac or Linux"
   exit 0
 fi
 
+if [ "$EUID" -ne 0 ]; then
+  isRoot=0
+else
+  isRoot=1
+fi
+
 echo "System Check Pass"
 
-function isRoot() {
-  if [ "$EUID" -ne 0 ]; then
-    return 1
-  else
-    return 0
-fi
-}
-
-# if isRoot; then
-  # echo "root"
-# else
-  # echo "no root"
-# fi
-
-# exit 0
-
-
-# auto env script
+# Auto env script
 cd ~
-
 export ORIGIN_PWD=`pwd`
 
 # exvim
@@ -69,12 +45,16 @@ if [ ! -d ".exvim" ]; then
 
   echo "exvim[4/4] update plugins"
   vim +PluginInstall +PluginUpdate +qall
+else
+  echo "exvim Installed"
 fi
 
 # brew
-if isOSX && ! hash brew 2>/dev/null; then
+if [ ${isOSX} = 1 ] && [ ! hash brew 2>/dev/null ]; then
   echo "Install Brew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "No Brew Install"
 fi
 
 # System
@@ -84,32 +64,36 @@ fi
 
 # exvim
 # gawk, ctags, cscope, idutils, sed
-if isLinux; then
-  if isRoot; then
+if [ ${isLinux} = 1 ]; then
+  if [ ${isRoot} = 1 ]; then
     sudo -HE apt-get install axel silversearcher-ag jq
 
     sudo -HE apt-get install gawk ctags idutils cscope
   else
-    source_path="/home/liyunkai/local/source"
     echo "install from source"
+    source_path="/home/liyunkai/local/source"
+    install_path="/home/liyunkai/local"
     echo "download source in "${source_path}
-    if [ -d ${source_path} ]; then
-      mkdir ${source_path}
+    if [ ! -d ${source_path} ]; then
+      mkdir -p ${source_path}
+    fi
+    
+    if [ ! -d ${source_path} ]; then
+      echo "${source_path} does not exist"
+      exit 1
     fi
 
-    wget https://github.com/axel-download-accelerator/axel/releases/download/v2.16.1/axel-2.16.1.tar.gz -o ${source_path}/axel.tar.gz
-    wget https://geoff.greer.fm/ag/releases/the_silver_searcher-2.2.0.tar.gz -o ${source_path}/ag.tar.gz
-    wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz -o ${source_path}/jq.tar.gz
+    wget https://github.com/axel-download-accelerator/axel/releases/download/v2.16.1/axel-2.16.1.tar.gz -O ${source_path}/axel.tar.gz
+    wget https://geoff.greer.fm/ag/releases/the_silver_searcher-2.2.0.tar.gz -O ${source_path}/ag.tar.gz
+    wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-1.6.tar.gz -O ${source_path}/jq.tar.gz
 
-
-    wget http://ftp.gnu.org/gnu/gawk/gawk-4.2.1.tar.gz -o ${source_path}/gawk.tar.gz
-    wget http://prdownloads.sourceforge.net/ctags/ctags-5.8.tar.gz -o ${source_path}/ctags.tar.gz
-    wget https://downloads.sourceforge.net/project/cscope/cscope/v15.9/cscope-15.9.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fcscope%2Ffiles%2Fcscope%2Fv15.9%2F&ts=1548846850&use_mirror=jaist -o ${source_path}/cscope.tar.gz
-    wget https://downloads.sourceforge.net/project/gnuwin32/id-utils/4.0-2/id-utils-4.0-2-bin.zip?r=http%3A%2F%2Fgnuwin32.sourceforge.net%2Fpackages%2Fid-utils.htm&ts=1548847179&use_mirror=excellmedia -o ${source_path}/idutils.zip
-    wget https://downloads.sourceforge.net/project/gnuwin32/sed/4.2.1/sed-4.2.1-src.zip?r=http%3A%2F%2Fgnuwin32.sourceforge.net%2Fpackages%2Fsed.htm&ts=1548847171&use_mirror=jaist -o ${source_path}/sed.zip
-
+    wget http://ftp.gnu.org/gnu/gawk/gawk-4.2.1.tar.gz -O ${source_path}/gawk.tar.gz
+    wget http://prdownloads.sourceforge.net/ctags/ctags-5.8.tar.gz -O ${source_path}/ctags.tar.gz
+    wget https://downloads.sourceforge.net/project/cscope/cscope/v15.9/cscope-15.9.tar.gz -O ${source_path}/cscope.tar.gz
+    wget https://downloads.sourceforge.net/project/gnuwin32/id-utils/4.0-2/id-utils-4.0-2-bin.zip -O ${source_path}/idutils.zip
+    wget https://downloads.sourceforge.net/project/gnuwin32/sed/4.2.1/sed-4.2.1-src.zip -O ${source_path}/sed.zip
   fi
-elif isOSX; then
+elif [ ${isOSX} = 1 ]; then
   brew install axel the_silver_searcher jq
 
   brew install macvim --with-cscope --with-lua --HEAD
