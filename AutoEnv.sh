66540b332ddc136595db0804ca97b6155757fc19
Yunkai Li <yunkai.li@hotmail.com>
2019-01-30 18:15:37 +0800

    add system support

--- AutoEnv.sh
+++ AutoEnv.sh
@@ -1,31 +1,71 @@
 #!/bin/bash
 
+# Check System
+case "$(uname -s)" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+# isOSX
+function isOSX() {
+  if [ ${machine} = 'Mac' ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# isLinux
+function isLinux() {
+  if [ ${machine} = 'Mac' ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if ( ! isOSX ) && ( ! isLinux ); then
+  echo "Support Mac or Linux"
+  exit 0
+fi
+
+echo "System Check Pass"
+
 # auto env script
 cd ~
 
 export ORIGIN_PWD=`pwd`
 
 # exvim
-echo "exvim[1/4] cloning repo"
 if [ ! -d ".exvim" ]; then
-    git clone https://github.com/yunkaili/main .exvim
+  echo "exvim[1/4] cloning repo"
+  git clone https://github.com/yunkaili/main .exvim
+  cd .exvim
+
+  echo "exvim[2/4] build vimrc"
+  echo "let g:exvim_custom_path='~/.exvim/'
+  source ~/.exvim/.vimrc" > ~/.vimrc
+
+  echo "exvim[3/4] install vundle"
+  bash unix/install.sh
+
+  echo "exvim[4/4] update plugins"
+  vim +PluginInstall +PluginUpdate +qall
 fi
-cd .exvim
-
-echo "exvim[2/4] build vimrc"
-#bash unix/vim.sh
-echo "let g:exvim_custom_path='~/.exvim/'
-source ~/.exvim/.vimrc" > ~/.vimrc
-
-echo "exvim[3/4] install vundle"
-bash unix/install.sh
-
-echo "exvim[4/4] update plugins"
-vim +PluginInstall +PluginUpdate +qall
 
 # brew
+if isOSX; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # axel
+# unix
+# sudo -HE apt-get install axel
+# osx
+# brew install axel
+# from source
+#
 
 # silversearcher-ag
 # unix
@@ -55,3 +95,5 @@ vim +PluginInstall +PluginUpdate +qall
 cd ${ORIGIN_PWD}
 
 # YouCompleteMe
+
+
